a2c053c40872b2495691ec7b0864310229693ff7
wangzuke <wangzuke2002@163.com>
2026-07-11 21:41:05 +0800

    Use experiment env for r0711 launcher

--- missing_ts_exp/scripts/run_r0711.sh
+++ missing_ts_exp/scripts/run_r0711.sh
@@ -11,5 +11,10 @@ CMD_FILE=${1:-scripts/r0711_cmds.txt}
 GPU_LIST=${GPU_LIST:-1,2,3,4,5,6,7}
 PER_GPU=${PER_GPU:-1}
 LOG_DIR=${LOG_DIR:-logs/r0711}
+PYTHON_ENV_BIN=${PYTHON_ENV_BIN:-/data/miniconda3/envs/itransformer/bin}
+
+if [[ -d "$PYTHON_ENV_BIN" ]]; then
+    export PATH="$PYTHON_ENV_BIN:$PATH"
+fi
 
 bash scripts/run_experiments.sh "$CMD_FILE" "$GPU_LIST" "$PER_GPU" "$LOG_DIR"
